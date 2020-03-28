e938c49ed5afef3f0d833617cced94a9179cdddd
Jorawar Singh <kawal5_nijjar@yahoo.co.in>
2020-03-28 18:10:27 -0400

    db added

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
-const dbConnection = mysql.createConnection({
+const dbConnection = mysql.createPool({
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'bde2175ab374cb',
     password: 'cabc2fd4',
+    db: "heroku_d78b44498ae6bea"
 });
 
 
