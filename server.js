12348dd157e497bd96d4d90eec9cc5fe95416527
Jorawar Singh <kawal5_nijjar@yahoo.co.in>
2020-03-28 20:49:31 -0400

    req.query fix

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/patient', (req, res, next) => {
 
 //DELETE SINGLE patient
 app.delete('/patient', (req, res, next) => {
-    let patientId = req.params.id;
+    let patientId = req.query.id;
     console.log(req);
     if(!patientId){
         return res.status(400).send({error: true, message: 'Please provide a patient id'});
